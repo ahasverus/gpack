cf5d95455f2b53cc5b780aeadd643522b36d8b0f
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-02-01 16:11:58 +0100

    Add get_ua() + Update Description

--- R/change_ua.R
+++ R/change_ua.R
@@ -1,6 +1,10 @@
-#' @title Get a Random User Agents
+#' @title Get a Random User Agent
 #'
-#' @description This function randomly selects a user agent among a list of 81 obtained from \url{https://techblog.willshouse.com/2012/01/03/most-common-user-agents/} (Last Updated: Fri, 31 Jan 2020).
+#' @description This function randomly selects a user agent among a list of 81
+#' obtained from \url{https://techblog.willshouse.com/2012/01/03/most-common-user-agents/}.
+#' The new user agent is checked using the What's My UA API (\url{https://www.whatsmyua.info/api/v1/ua}).
+#'
+#' @param verbose A boolean. If TRUE, the new user agent is printed.
 #'
 #' @return The selected user agent.
 #'
@@ -15,7 +19,13 @@
 
 
 
-change_ua <- function() {
+change_ua <- function(verbose = TRUE) {
 
-  as.character(sample(uagents[ , "user_agent"], 1))
+  agent <- as.character(sample(agents[ , "user_agent"], 1))
+
+  if (verbose) {
+    get_ua(agent)
+  }
+
+  return(agent)
 }
