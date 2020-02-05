ce687ed74f72e3fcb70b7713efe3128bfe6d2137
Nicolas Casajus <nicolas.casajus@gmail.com>
2020-02-05 10:56:56 +0100

    Change error message (stick + ui_stop) + Update Doc

--- R/get_ua.R
+++ R/get_ua.R
@@ -6,6 +6,7 @@
 #' @param agent A character of length 1 specifying a user agent.
 #' @param verbose A boolean. If TRUE, the new user agent is printed.
 #'
+#' @import usethis
 #' @import httr
 #' @import jsonlite
 #'
@@ -16,8 +17,11 @@
 #' @examples
 #' \dontrun{
 #' get_ua()
-#' 
+#'
 #' ua <- change_ua()
+#' get_ua(agent = ua)
+#'
+#' ua <- change_ua("Mozilla/5.0 (Linux x86_64) Gecko/20100101 Firefox/72.0")
 #' get_ua(agent = ua)
 #' }
 
@@ -28,15 +32,45 @@ get_ua <- function(agent, verbose = TRUE) {
   if (!missing(agent)) {
 
     if (is.null(agent)) {
-      stop("Argument 'agent' cannot be NULL.")
+
+      usethis::ui_stop(
+        stick(
+          "
+            Argument
+            {usethis::ui_field('agent')}
+            cannot be
+            {usethis::ui_value('NULL')}
+          "
+        )
+      )
     }
 
     if (length(agent) != 1) {
-      stop("Argument 'agent' must be a character of length 1.")
+
+      usethis::ui_stop(
+        stick(
+          "
+            Argument
+            {usethis::ui_field('agent')}
+            must be a
+            {usethis::ui_value('character of length 1')}
+          "
+        )
+      )
     }
 
     if (!is.character(agent)) {
-      stop("Argument 'agent' must be a character of length 1.")
+
+      usethis::ui_stop(
+        stick(
+          "
+            Argument
+            {usethis::ui_field('agent')}
+            must be a
+            {usethis::ui_value('character of length 1')}
+          "
+        )
+      )
     }
 
     ua_sent  <- httr::user_agent(agent)
@@ -48,7 +82,15 @@ get_ua <- function(agent, verbose = TRUE) {
   }
 
   if (response$status_code != 200) {
-    stop("Unable to resolve host: https://www.whatsmyua.info.")
+
+    usethis::ui_stop(
+      stick(
+        "
+          Unable to resolve host
+          {usethis::ui_value('https://www.whatsmyua.info')}
+        "
+      )
+    )
   }
 
   response <- httr::content(response, as = "text")
@@ -57,12 +99,30 @@ get_ua <- function(agent, verbose = TRUE) {
   ua_short <- jsonlite::fromJSON(response)[3, "device"]$"description"
 
   if (!missing(agent)) {
+
     if (agent != ua_long) {
-      stop("Unable to change user-agent.")
+
+      usethis::ui_stop(
+        stick(
+          "
+            Unable to change
+            {usethis::ui_value('user-agent')}
+          "
+        )
+      )
     }
   }
 
   if (verbose) {
-    cat("Your system is now detected as:", ua_short, "\n")
+
+    usethis::ui_done(
+      stick(
+        "
+          Your system is now detected as
+          {usethis::ui_value(ua_short)}
+        ",
+        indent = " "
+      )
+    )
   }
 }
